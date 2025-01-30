1be643c67458c4911ac03464291ce0ea0425aee5
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-01-30 14:29:22 -0500

    "Task 2 - Modifying Customer Orders."

--- cc_3.js
+++ cc_3.js
@@ -1,7 +1,14 @@
 //"Task 1 - Product Price Management."
 let prices = [1.99, 2.99, 3.99, 4.99, 5.99];
-prices.push (6.99)
-console.log(prices)
+prices.push (6.99);
+console.log(prices);
 prices.shift();
-let updatedprice = prices
-console.log(updatedprice)
+let updatedprice = prices;
+console.log("Updated price after removing first value:", updatedprice);
+
+//  "Task 2 - Modifying Customer Orders."
+let quantities = [10, 20, 30, 40, 50];
+quantities [2] += 5; //adding 5 to the third value
+let total_quantities = quantities.reduce((sum, total)=> sum + total,0);
+
+console.log("Total order count:", total_quantities); 
