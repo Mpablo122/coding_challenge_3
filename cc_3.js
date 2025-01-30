3a5589a02de3ab9d040b17f6dd1d72bd12b13b08
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-01-30 14:16:52 -0500

    "Task 1 - Product Price Management."

---
+++ cc_3.js
@@ -0,0 +1,7 @@
+//"Task 1 - Product Price Management."
+let prices = [1.99, 2.99, 3.99, 4.99, 5.99];
+prices.push (6.99)
+console.log(prices)
+prices.shift();
+let updatedprice = prices
+console.log(updatedprice)
